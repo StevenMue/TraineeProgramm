0ad03603f79abf8ecf86d4afa7d903e74da2f81a
StevenMue <mueller.steven@posteo.de>
2022-04-27 13:53:14 +0200

    Updated the pom.xml Added documentation to the Controller. Controller: player handling bugfix.

--- javaSchulung/src/main/java/de/materna/candygame/Controller.java
+++ javaSchulung/src/main/java/de/materna/candygame/Controller.java
@@ -7,6 +7,7 @@ import de.materna.candygame.events.Event;
 import de.materna.candygame.events.Gift;
 import de.materna.candygame.events.RandomNegativeTravelAmount;
 import de.materna.candygame.events.Thief;
+import static org.mockito.BDDMockito.*;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -21,8 +22,8 @@ public class Controller {
     boolean returnFlag = false;
     private UIConsoleGenerator ui = new UIConsoleGenerator();
     PlayerStates state;
+    Player currentPlayer;
 
-    //TODO move to city
     Random r;
     int[] candys = new int[Candy.values().length];
     StringBuilder sb = new StringBuilder();
@@ -30,6 +31,10 @@ public class Controller {
     private Controller() {
     }
 
+    /**
+     * Constructor for initialise the game and its classes
+     * @param amountOfPlayer the number of players in the game
+     */
     Controller(int amountOfPlayer) {
         for (int i = 0; i < amountOfPlayer; i++) {
             players.add(new Player(2000, CityENUM.CENTRAL));
@@ -39,13 +44,19 @@ public class Controller {
         r = new Random();
     }
 
+    /**
+     * Get the user input.
+     * @return user input as a string
+     */
     private String getUserInput() {
         return new Scanner(System.in).nextLine();
     }
 
+    /**
+     * Check the user input if its one of the options and activate wanted action.
+     * @param input string user input
+     */
     private void handler(String input) {
-
-        //TODO complete switch, refector names "want..."
         switch (input.toLowerCase(Locale.ROOT)) {
             case "t" -> travel();
             case "b" -> bankOrBuy();
@@ -65,9 +76,14 @@ public class Controller {
         }
     }
 
+    /**
+     * Check the position and input of the player to decide which action to choose.
+     * proceed to the market if the player is in the player window.
+     * proceed with the store money action when the player is in the bank.
+     */
     private void marketOrBank() {
         if (state == PlayerStates.IDLE) {
-            ui.printMarketWindow(candys, players.get(0));
+            ui.printMarketWindow(candys, currentPlayer);
             state = PlayerStates.MARKET;
         } else if (state == PlayerStates.BANK) {
             storeMoney();
@@ -77,9 +93,14 @@ public class Controller {
         }
     }
 
+    /**
+     * Check the position and input of the player to decide which action to choose.
+     * Proceed to the bank if the player is in the player window.
+     * Proceed with the buy action when the player is in the market.
+     */
     private void bankOrBuy() {
-        if (state == PlayerStates.IDLE && players.get(0).getCurrentCityENUM() == bank.homecity) {
-            ui.printBankWindow(bank, players.get(0));
+        if (state == PlayerStates.IDLE && currentPlayer.getCurrentCityENUM() == bank.homecity) {
+            ui.printBankWindow(bank, currentPlayer);
             state = PlayerStates.BANK;
         } else if (state == PlayerStates.MARKET) {
             buy();
@@ -88,69 +109,92 @@ public class Controller {
         }
     }
 
+    /**
+     * Check input of the player to decide which action to choose.
+     * Proceed with the store items action when the player is in the bank.
+     */
     private void wantToStoreItems() {
         if (state == PlayerStates.BANK) {
-            ui.printBankWindow(bank, players.get(0));
+            ui.printBankWindow(bank, currentPlayer);
             storeItems();
         } else {
             ui.message("Pls enter a valid key.");
         }
     }
 
+    /**
+     * Check input of the player to decide which action to choose.
+     * Proceed with the take money action when the player is in the bank.
+     */
     private void wantToTakeMoney() {
         if (state == PlayerStates.BANK) {
-            ui.printBankWindow(bank, players.get(0));
+            ui.printBankWindow(bank, currentPlayer);
             takeMoney();
         } else {
             ui.message("Pls enter a valid key.");
         }
     }
 
+    /**
+     * Check input of the player to decide which action to choose.
+     * Proceed with the take credit action when the player is in the bank.
+     */
     private void wantToTakeCredit() {
         if (state == PlayerStates.BANK) {
-            ui.printBankWindow(bank, players.get(0));
+            ui.printBankWindow(bank, currentPlayer);
             takeCredit();
         } else {
             ui.message("Pls enter a valid key.");
         }
     }
 
+    /**
+     * Check input of the player to decide which action to choose.
+     * Proceed with the take items action when the player is in the bank.
+     */
     private void wantToTakeItems() {
         if (state == PlayerStates.BANK) {
-            ui.printBankWindow(bank, players.get(0));
+            ui.printBankWindow(bank, currentPlayer);
             takeItems();
         } else {
             ui.message("Pls enter a valid key.");
         }
     }
 
+    /**
+     * Check input of the player to decide which action to choose.
+     * Proceed with the repay debt action when the player is in the bank.
+     */
     private void wantToRepayDebt() {
         if (state == PlayerStates.BANK) {
-            ui.printBankWindow(bank, players.get(0));
+            ui.printBankWindow(bank, currentPlayer);
             repayDebt();
         } else {
             ui.message("Pls enter a valid key.");
         }
     }
 
-    //TODO make a new class market to handel that
+    /**
+     * Method to buy items in the market.
+     * User need to enter the ID of the candy and the amount.
+     */
     private void buy() {
         ui.message("What do you want to buy? : ");
 
         try {
             int itemID = Integer.parseInt(getUserInput());
-            ui.message("How much do you wish to buy? You can buy " + players.get(0).spaceLeft() + " : ");
+            ui.message("How much do you wish to buy? You can buy " + currentPlayer.spaceLeft() + " : ");
             int amount = Integer.parseInt(getUserInput());
-            if (players.get(0).spaceLeft() < amount) {
-                ui.errorMessage("You backpack cant hold so much! You have only " + players.get(0).spaceLeft() + " space left!");
+            if (currentPlayer.spaceLeft() < amount) {
+                ui.errorMessage("You backpack cant hold so much! You have only " + currentPlayer.spaceLeft() + " space left!");
                 getUserInput();
                 returnFlag = true;
                 return;
             }
-            TaskCompletionState task = players.get(0).reduceMoney(amount * candys[itemID]);
+            TaskCompletionState task = currentPlayer.reduceMoney(amount * candys[itemID]);
 
             if (task.isSuccess) {
-                task = players.get(0).addItem(Candy.getCandy(itemID), amount);
+                task = currentPlayer.addItem(Candy.getCandy(itemID), amount);
                 if (!task.isSuccess) {
                     ui.errorMessage(task.msg);
                     getUserInput();
@@ -171,15 +215,19 @@ public class Controller {
 
     }
 
+    /**
+     * Method to sell items in the market.
+     * User need to enter the ID of the candy and the amount.
+     */
     private void sell() {
         ui.message("What do you want to sell? : ");
         try {
             int itemID = Integer.parseInt(getUserInput());
-            ui.message("How much do you wish to sell? You have " + players.get(0).getItem(itemID) + " : ");
+            ui.message("How much do you wish to sell? You have " + currentPlayer.getItem(itemID) + " : ");
             int amount = Integer.parseInt(getUserInput());
-            TaskCompletionState task = players.get(0).reduceItem(Candy.getCandy(itemID), amount);
+            TaskCompletionState task = currentPlayer.reduceItem(Candy.getCandy(itemID), amount);
             if (task.isSuccess) {
-                players.get(0).addMoney(amount * candys[itemID]);
+                currentPlayer.addMoney(amount * candys[itemID]);
             } else {
                 ui.errorMessage(task.msg);
                 getUserInput();
@@ -195,13 +243,17 @@ public class Controller {
         }
     }
 
+    /**
+     * Method to store items in the account.
+     * User need to enter the ID of the candy and the amount.
+     */
     private void storeItems() {
         ui.message("What do you want to store?");
         try {
             int itemID = Integer.parseInt(getUserInput());
-            ui.message("How much do you wish to store? You have " + players.get(0).getItem(itemID));
+            ui.message("How much do you wish to store? You have " + currentPlayer.getItem(itemID));
             int amount = Integer.parseInt(getUserInput());
-            TaskCompletionState task = bank.storeItems(players.get(0), Candy.getCandy(itemID), amount);
+            TaskCompletionState task = bank.storeItems(currentPlayer, Candy.getCandy(itemID), amount);
             if (!task.isSuccess) {
                 ui.errorMessage(task.msg);
                 getUserInput();
@@ -217,6 +269,10 @@ public class Controller {
         }
     }
 
+    /**
+     * Method to take items from the account.
+     * User need to enter the ID of the candy and the amount.
+     */
     private void takeItems() {
         ui.message("What do you want to take?");
         TaskCompletionState task;
@@ -225,9 +281,9 @@ public class Controller {
             int itemID = Integer.parseInt(getUserInput());
             ui.message("How much do you wish to take?");
             int amount = Integer.parseInt(getUserInput());
-            if (players.get(0).spaceLeft() >= amount) {
+            if (currentPlayer.spaceLeft() >= amount) {
                 ui.message("You dont have enough left space!");
-                task = bank.takeItems(players.get(0), Candy.getCandy(itemID), amount);
+                task = bank.takeItems(currentPlayer, Candy.getCandy(itemID), amount);
                 if (!task.isSuccess) {
                     ui.errorMessage(task.msg);
                     getUserInput();
@@ -243,11 +299,15 @@ public class Controller {
         }
     }
 
+    /**
+     * Method to store money in the account.
+     * User need to enter the amount.
+     */
     private void storeMoney() {
         try {
             ui.message("How much do you wish to store?");
             int amount = Integer.parseInt(getUserInput());
-            TaskCompletionState task = bank.storeMoney(players.get(0), amount);
+            TaskCompletionState task = bank.storeMoney(currentPlayer, amount);
             if (!task.isSuccess) {
                 ui.errorMessage(task.msg);
                 getUserInput();
@@ -260,11 +320,15 @@ public class Controller {
         }
     }
 
+    /**
+     * Method to take money from the account.
+     * User need to enter  the amount.
+     */
     private void takeMoney() {
         try {
             ui.message("How much do you wish to take?");
             int amount = Integer.parseInt(getUserInput());
-            TaskCompletionState task = bank.takeMoney(players.get(0), amount);
+            TaskCompletionState task = bank.takeMoney(currentPlayer, amount);
             if (!task.isSuccess) {
                 ui.errorMessage(task.msg);
                 getUserInput();
@@ -277,24 +341,33 @@ public class Controller {
         }
     }
 
+    /**
+     * Method to return to the player window.
+     */
     private void goBack() {
         returnFlag = true;
         state = PlayerStates.IDLE;
     }
 
+    /**
+     * Method to end the game and show the scoreboard.
+     */
     private void leave() {
         currentDay = 31;
     }
 
-    //TODO create event
-
+    /**
+     * Method to travel to another city.
+     * Check if an event will occur.
+     * User need to enter the ID of the city.
+     */
     private void travel() {
         int[] costs = new int[CityENUM.values().length];
 
         for (CityENUM city : CityENUM.values()) {
             costs[city.getID()] = calculateTravelCosts(city);
         }
-        ui.printTravelTargets(costs, players.get(0), currentDay);
+        ui.printTravelTargets(costs, currentPlayer, currentDay);
         try {
             String input;
                 input = getUserInput();
@@ -302,15 +375,13 @@ public class Controller {
                     returnFlag = true;
                     return;
                 }
-            //TODO optimize for more player
-            Player player = players.get(0);
             if(Integer.parseInt(input)>(CityENUM.values().length-1)||Integer.parseInt(input)<0){
                 throw new IllegalArgumentException();
             }
-            player.travel(CityENUM.getCity(Integer.parseInt(input)), costs[Integer.parseInt(input)]);
+            currentPlayer.travel(CityENUM.getCity(Integer.parseInt(input)), costs[Integer.parseInt(input)]);
             if (Math.random() <= 0.1) {
                 Event event = createEvent();
-                TaskCompletionState task = event.process(player);
+                TaskCompletionState task = event.process(currentPlayer);
                 if (task.isSuccess) {
                     currentDay += event.getDuration();
                     ui.errorMessage(event.getMessage());
@@ -329,11 +400,15 @@ public class Controller {
 
     }
 
+    /**
+     * Method to take a credit from the bank.
+     * User need to enter the amount.
+     */
     private void takeCredit() {
         ui.message("How much credit do you want to take?");
         try {
             int input = Integer.parseInt(getUserInput());
-            bank.giveCredit(players.get(0), input);
+            bank.giveCredit(currentPlayer, input);
             returnFlag = true;
         } catch (NumberFormatException e) {
             ui.errorMessage(YOU_DONT_ENTERED_A_NUMBER_TRY_AGAIN);
@@ -341,11 +416,15 @@ public class Controller {
         }
     }
 
+    /**
+     * Method to repay the debt to the bank.
+     * User need to enter the amount.
+     */
     private void repayDebt() {
         ui.message("How much debt do you want to repay?");
         try {
             int input = Integer.parseInt(getUserInput());
-            bank.reduceDebt(players.get(0), input);
+            bank.reduceDebt(currentPlayer, input);
             returnFlag = true;
         } catch (NumberFormatException e) {
             ui.errorMessage(YOU_DONT_ENTERED_A_NUMBER_TRY_AGAIN);
@@ -353,14 +432,16 @@ public class Controller {
         }
     }
 
-    //TODO optimize for more player
-    //TODO move to City
     private int calculateTravelCosts(CityENUM city) {
-        Player player = players.get(0);
-        int costs = (int) Math.round(city.getDistance(player.getCurrentCityENUM()) * 10);
+        int costs = (int) Math.round(city.getDistance(currentPlayer.getCurrentCityENUM()) * 10);
         return costs;
     }
 
+    /**
+     * Check if the user traveled to end the current day
+     * on day end the bank interest charges will be added and
+     * the new candy costs will be calculated.
+     */
     private void nextDay() {
         if (!returnFlag) {
             bank.nextDay();
@@ -370,15 +451,23 @@ public class Controller {
         returnFlag = false;
     }
 
+    /**
+     * If more than 30 days passed the game will end and the scoreboard will be shown for each player
+     */
     private void endGame() {
         if (currentDay == 30) {
-            ui.printScoreBoard((players.get(0).getBalance()+bank.getPlayerAccount(players.get(0)).getPlayerAccountBalance()) -
-                    bank.getPlayerAccount(players.get(0)).getPlayerDebt());
+            ui.printScoreBoard((currentPlayer.getBalance()+bank.getPlayerAccount(currentPlayer).getPlayerAccountBalance()) -
+                    bank.getPlayerAccount(currentPlayer).getPlayerDebt());
             ui.message("Press enter");
             getUserInput();
         }
     }
 
+    /**
+     * Create a event based on occurs chance.
+     * @return Event will return the occurred event
+     * @see Event
+     */
     private Event createEvent() {
         int chance = r.nextInt(0, 100);
         if (chance < 30) {
@@ -390,13 +479,14 @@ public class Controller {
         }
     }
 
-    //TODO move to City
+    /**
+     * Method to calculate the candy cost of a city
+     */
     private void randomCityCosts() {
         for (Candy candy : Candy.values()) {
             this.candys[candy.getID()] = r.nextInt(candy.minPrice(), candy.maxPrice());
         }
     }
-
 
     /**
      * Function for running the game
@@ -404,14 +494,17 @@ public class Controller {
     public void run() {
         this.randomCityCosts();
         while (this.currentDay < 30) {
+            for (Player player: players) {
+                currentPlayer=player;
             switch (state.name()) {
-                case "BANK" -> ui.printBankWindow(bank, players.get(0));
-                case "MARKET" -> ui.printMarketWindow(candys, players.get(0));
-                default -> ui.printPlayerWindow(players.get(0),
-                        players.get(0).getCurrentCityENUM() == bank.homecity);
+                case "BANK" -> ui.printBankWindow(bank, currentPlayer);
+                case "MARKET" -> ui.printMarketWindow(candys, currentPlayer);
+                default -> ui.printPlayerWindow(currentPlayer,
+                        currentPlayer.getCurrentCityENUM() == bank.homecity);
             }
             this.handler(this.getUserInput());
             this.nextDay();
+            }
         }
         this.endGame();
     }
